6dd5c384fb71eb848dced3d8b64e53061de3dbdb
PAULO H PEDROSO <120056195+phpedroso@users.noreply.github.com>
2023-06-12 15:17:06 -0300

    Adicionando arquivos do exercicio.

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicacao(10, 3));
+console.log(saudacao('PHP'));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+  }
+  
+  function saudacao(nome: string): string {
+    return "Olá " + nome;
+  }
+
+  console.log(multiplicacao(10, 3));
+  console.log(saudacao('PHP'));
